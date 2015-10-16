79dcae5844cb165158ff2ec639cce48b477515b7
Alex Heyd <alex@gaikai.com>
2015-10-15 22:22:49 -0700

    Added notification animation and hacky automation script for chat data.

--- app/components/route-application/component.js
+++ app/components/route-application/component.js
@@ -11,6 +11,26 @@ export default Ember.Component.extend({
 
   classNames: ['application', 'container-fluid'],
 
+  //************************************************************************
+  // TODO: REMOVE DEV CODE
+  didInsertElement() {
+    this.get('twitch').on('join', this.onChannelJoin.bind(this));
+  },
+
+  onChannelJoin() {
+    let _say = function (msg) {
+      this.get('twitch').say(msg || 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
+    }.bind(this);
+
+    _say();
+
+    Ember.run.later(_say.bind(this, '@GhostCryptology hi'), 1000);
+    Ember.run.later(_say.bind(this), 2000);
+    Ember.run.later(_say.bind(this, '@GhostCryptology hello'), 3000);
+  },
+  // END DEV CODE
+  //************************************************************************
+
   onTwitchConnection: function () {
     let connecting = this.get('twitch.connecting');
 
@@ -27,6 +47,12 @@ export default Ember.Component.extend({
     if (fetchingEmotes) {
       this.get('notifications').info('Fetching Twitch Emojis... Please wait...');
     } else {
+      //************************************************************************
+      // TODO: REMOVE DEV CODE
+      this.get('twitch').say('dvrsPUP');
+      // END DEV CODE
+      //************************************************************************
+
       this.get('notifications').remove();
     }
 
