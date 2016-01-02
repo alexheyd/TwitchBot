8a6a24886cd077698821ebf921aeed0148f84757
Alex Heyd <alex@gaikai.com>
2016-01-02 01:15:07 -0800

    Visual updates.

--- app/components/route-lists/component.js
+++ app/components/route-lists/component.js
@@ -1,4 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['lists'],
+  lists: [],
+  newListName: '',
+
+  actions: {
+    createList() {
+      let newName = this.get('newListName');
+
+      if (newName && typeof newName === 'string' && !this.hasList(newName)) {
+        this.addList({
+          name: newName
+        });
+
+        this.set('newListName', '');
+      }
+    }
+  },
+
+  hasList(name) {
+    let list = this.get('lists').findBy('name', name);
+    return !!list;
+  },
+
+  addList(list) {
+    if (list && typeof list === 'object') {
+      this.get('lists').pushObject(list);
+    }
+  }
 });
